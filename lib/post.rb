4b77be2d8bbe0577186ebad1ed3d921bbc30f3e8
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 15:54:43 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+ 
+  attr_accessor :title 
+ 
+  def initialize(title)
+    @title = title
+  end
+end
+
+oliver_twist = Post.new("Oliver Twist")
+
+post.author.name 
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+ 
+  attr_accessor :title
+ 
+  def initialize(title)
+    @title = title
+  end
+end
+
+real_slim_shady = Song.new("Real Slim Shady")
+
+song.artist.name 
